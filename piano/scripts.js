81176429539a7827f9a59be6ed5a0700f6ea460a
Daniel Kenzi <danirods90@gmail.com>
2019-04-18 01:38:57 -0300

    Add files via upload

---
+++ piano/scripts.js
@@ -0,0 +1,173 @@
+const C4 = new Audio("sounds/C4.mp3");
+const Db4 = new Audio("sounds/Db4.mp3");
+const D4 = new Audio("sounds/D4.mp3");
+const Eb4 = new Audio("sounds/Eb4.mp3");
+const E4 = new Audio("sounds/E4.mp3");
+const F4 = new Audio("sounds/F4.mp3");
+const Gb4 = new Audio("sounds/Gb4.mp3"); 
+const G4 = new Audio("sounds/G4.mp3");
+const Ab4 = new Audio("sounds/Ab4.mp3");
+const A4 = new Audio("sounds/A4.mp3");
+const Bb4 = new Audio("sounds/Bb4.mp3");
+const B4 = new Audio("sounds/B4.mp3");
+const C5 = new Audio("sounds/C5.mp3");
+const Db5 = new Audio("sounds/Db5.mp3");
+const D5 = new Audio("sounds/D5.mp3");
+const Eb5 = new Audio("sounds/Eb5.mp3");
+const E5 = new Audio("sounds/E5.mp3");
+
+
+const C4key = document.querySelector(".C4-key");
+
+function playC4 () {
+  C4.load();
+  C4.play();
+}
+
+C4key.addEventListener ("click", playC4);
+
+
+const Db4key = document.querySelector(".Db4-key");
+
+function playDb4 () {
+  Db4.load();
+  Db4.play();
+}
+
+Db4key.addEventListener ("click", playDb4);
+
+const D4key = document.querySelector(".D4-key");
+
+function playD4 () {
+  D4.load();
+  D4.play();
+}
+
+D4key.addEventListener ("click", playD4);
+
+const Eb4key = document.querySelector(".Eb4-key");
+
+function playEb4 () {
+  Eb4.load();
+  Eb4.play();
+}
+
+Eb4key.addEventListener ("click", playEb4);
+
+const E4key = document.querySelector(".E4-key");
+
+function playE4 () {
+  E4.load();
+  E4.play();
+}
+
+E4key.addEventListener ("click", playE4);
+
+const F4key = document.querySelector(".F4-key");
+
+function playF4 () {
+  F4.load();
+  F4.play();
+}
+
+F4key.addEventListener ("click", playF4);
+
+const Gb4key = document.querySelector(".Gb4-key");
+
+function playGb4 () {
+  Gb4.load();
+  Gb4.play();
+}
+
+Gb4key.addEventListener ("click", playGb4);
+
+const G4key = document.querySelector(".G4-key");
+
+function playG4 () {
+  G4.load();
+  G4.play();
+}
+
+G4key.addEventListener ("click", playG4);
+
+const Ab4key = document.querySelector(".Ab4-key");
+
+function playAb4 () {
+  Ab4.load();
+  Ab4.play();
+}
+
+Ab4key.addEventListener ("click", playAb4);
+
+const A4key = document.querySelector(".A4-key");
+
+function playA4 () {
+  A4.load();
+  A4.play();
+}
+
+A4key.addEventListener ("click", playA4);
+
+const Bb4key = document.querySelector(".Bb4-key");
+
+function playBb4 () {
+  Bb4.load();
+  Bb4.play();
+}
+
+Bb4key.addEventListener ("click", playBb4);
+
+const B4key = document.querySelector(".B4-key");
+
+function playB4 () {
+  B4.load();
+  B4.play();
+}
+
+B4key.addEventListener ("click", playB4);
+
+const C5key = document.querySelector(".C5-key");
+
+function playC5 () {
+  C5.load();
+  C5.play();
+}
+
+C5key.addEventListener ("click", playC5);
+
+
+const Db5key = document.querySelector(".Db5-key");
+
+function playDb5 () {
+  Db5.load();
+  Db5.play();
+}
+
+Db5key.addEventListener ("click", playDb5);
+
+
+const D5key = document.querySelector(".D5-key");
+
+function playD5 () {
+  D5.load();
+  D5.play();
+}
+
+D5key.addEventListener ("click", playD5);
+
+const Eb5key = document.querySelector(".Eb5-key");
+
+function playEb5 () {
+  Eb5.load();
+  Eb5.play();
+}
+
+Eb5key.addEventListener ("click", playEb5);
+const E5key = document.querySelector(".E5-key");
+
+function playE5 () {
+  E5.load();
+  E5.play();
+}
+
+E5key.addEventListener ("click", playE5);
